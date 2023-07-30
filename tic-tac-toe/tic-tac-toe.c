b71a36bee8f3d97043dd4b7780c16bbb8dc04c33
Neyko Naydenov <shendamere@gmail.com>
2023-07-30 13:46:45 +0300

    refactored, added user input and display of board

--- tic-tac-toe/tic-tac-toe.c
+++ tic-tac-toe/tic-tac-toe.c
@@ -1,11 +1,27 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define LOWER_CASE_O 111
 #define LOWER_CASE_X 120
 #define TO_UPPER(X) (X-= 32)
+#define ROWS 3
+#define COLUMNS 3
 
 
+typedef struct
+{
+	int playerRow;
+	int playerCol;
+	char input;
+} player;
 
+typedef struct
+{
+	int aiRow;
+	int aiCol;
+	char input;
+} ai;
 
 char take_input()
 {
@@ -20,45 +36,78 @@ char take_input()
 	return counter == 1 ? prev : -1;
 }
 
+void printBoard(char board[ROWS][COLUMNS])
+{
+	printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+	printf("\n---|---|---\n");
+	printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+	printf("\n---|---|---\n");
+	printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+	printf("\n");
+}
+/*
+void aiTurn()
+{
+	srand((unsigned int)(time(0)));
+	int pos
+}
+*/
+
 int main()
 {
-	char board[3][3] = { {' ',' ',' '}, {' ',' ', ' '}, {' ',' ',' '} };
+	char board[ROWS][COLUMNS] = { {' ',' ',' '}, {' ',' ', ' '}, {' ',' ',' '} };
 	int colSize = sizeof(board[0]) / sizeof(board[0][0]);
 	int rowSize = sizeof(board) / sizeof(board[0]);
-	char player, ai = ' ';
+	player player;
+	ai ai;
 	char isOver = 0;
 	printf("Which symbol would you use? X or O\n");
-	player = take_input();
-	while ((player != 'X' && player != 'O') && (player != 'x' && player != 'o'))
+	player.input = take_input();
+	while ((player.input != 'X' && player.input != 'O') && (player.input != 'x' && player.input != 'o'))
 	{
 		printf("Invalid input, please try again.\n");
-		player = take_input();
+		player.input = take_input();
 	}
-	if (player == LOWER_CASE_X || player == LOWER_CASE_O)
+	if (player.input == LOWER_CASE_X || player.input == LOWER_CASE_O)
 	{
-		TO_UPPER(player);
+		TO_UPPER(player.input);
 	}
 
-	switch (player)
+	switch (player.input)
 	{
-	case 'X': ai = 'O';
+	case 'X': ai.input = 'O';
 		break;
-	case 'O': ai = 'X';
+	case 'O': ai.input = 'X';
 		break;
 	}
-	printf("AI Picks: %c\n", ai);
+	printf("AI Picks: %c\n", ai.input);
 	while (!isOver)
 	{
-		printf("Please enter the spot where you'd like to place the character\nChoose an index between 1 and 9.\n");
-		player = take_input();
-	//	int pos = (player - 1) / rowSize;
+		printf("Please enter the spot where you'd like to place the character\nChoose an Row between 1 and 3.\n");
+		player.playerRow = (take_input() - '0');
+		printf("Please enter the spot where you'd like to place the character\nChoose an Col between 1 and 3.\n");
+		player.playerCol = (take_input() - '0');
+		while ((player.playerRow < 1) || (player.playerRow > 3) && (player.playerCol < 1) || (player.playerCol > 3))
+		{
+			printf("Invalid Column and Row please try again.\n");
+			player.input = take_input();
+		}
+
 		for (size_t rowIdx = 0; rowIdx < rowSize; ++rowIdx)
 		{
 			for (size_t colIdx = 0; colIdx < colSize; ++colIdx)
 			{
+				if ((player.playerRow == rowIdx) && (player.playerCol == colIdx))
+				{
+					board[player.playerRow][player.playerCol] = player.input;
+				}
 
 			}
 		}
+
+		aiTurn();
+
+		printBoard(board);
 	}
 }
 
